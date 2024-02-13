adb7b7235eb9cdf536b15b382787937d89deeb38
oliverm314 <yx23471@bristol.ac.uk>
2024-02-13 14:44:28 +0000

    Update operations.py

--- operations.py
+++ operations.py
@@ -1,1 +1,8 @@
 print("hello")
+
+def total(num1, num2):
+    total = int(num1) + int(num2)
+    return total
+num1 = input('please enter a number')
+num2 = input('please enter a number')
+print(total(num1, num2))
